1906a2f6f6e8293ba83341f6322cd43d78a8037a
Ruzaik Rafeek <ruzaik@passkit.com>
2018-06-20 23:17:03 +0530

    initialization

--- public/js/litebox.js
+++ public/js/litebox.js
@@ -1,7 +1,39 @@
 (function (global, factory) {
-	global.litebox = factory();
-}(window, function () {
-	function LiteBox() {
 
+	global.litebox = factory();
+
+}(window, function () {
+
+	function LiteBox() {
+		this.init();
 	}
+
+	Litebox.prototype.init = function () {
+		var self = this;
+
+		var init = function(){
+			console.log('init');
+			self.attachEventListener();
+			self.buildHTML();
+		};
+
+		if (document.readyState === "complete" || (document.readyState !== "loading" &&
+				!document.documentElement.doScroll)) {
+			init();
+		} else {
+			document.addEventListener("DOMContentLoaded", init);
+		}
+		
+	};
+	
+	LiteBox.prototype.attachEventListener = function () {
+		
+	};
+
+	LiteBox.prototype.buildHTML = function () {
+
+	};
+
+	return new LiteBox();
+
 }));
